a750e99c5d38a2cf0c60927b785766c1d6f047cc
らいとん <raitosongwei@gmail.com>
2025-08-02 04:44:04 +0900

    update: テストケースのアップデート

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,11 +7,12 @@ import {
   moneyFortunes,
   luckyColors,
   luckyItems,
+  luckyFoods, // ←追加
+  luckyNumbers, // ←追加
 } from './data';
 
 describe('omikuji-api', () => {
   it('レスポンスがOmikujiResponseSchemaでバリデーションを通る', () => {
-    // 疑似的にランダム生成（本番ロジックと同じ）
     const selected = fortunes[0];
     const comment = commentsMap[selected.fortune][0];
     const love = loveFortunes[0];
@@ -19,6 +20,8 @@ describe('omikuji-api', () => {
     const money = moneyFortunes[0];
     const luckyColor = luckyColors[0];
     const luckyItem = luckyItems[0];
+    const luckyFood = luckyFoods[0];
+    const luckyNumber = luckyNumbers[0];
     const date = '2025-08-02';
     const data = {
       fortune: selected.fortune,
@@ -27,12 +30,14 @@ describe('omikuji-api', () => {
       category: { love, work, money },
       lucky_color: luckyColor,
       lucky_item: luckyItem,
+      lucky_food: luckyFood,
+      lucky_number: luckyNumber,
       date,
     };
     expect(() => OmikujiResponseSchema.parse(data)).not.toThrow();
   });
 
-  it('fortune, comment, color, itemが定義リスト内の値である', () => {
+  it('fortune, comment, color, item, food, numberが定義リスト内の値である', () => {
     const selected = fortunes[2];
     const comment = commentsMap[selected.fortune][1];
     const love = loveFortunes[1];
@@ -40,6 +45,8 @@ describe('omikuji-api', () => {
     const money = moneyFortunes[1];
     const luckyColor = luckyColors[1];
     const luckyItem = luckyItems[1];
+    const luckyFood = luckyFoods[1];
+    const luckyNumber = luckyNumbers[1];
     const date = '2025-08-02';
     const data = {
       fortune: selected.fortune,
@@ -48,12 +55,16 @@ describe('omikuji-api', () => {
       category: { love, work, money },
       lucky_color: luckyColor,
       lucky_item: luckyItem,
+      lucky_food: luckyFood,
+      lucky_number: luckyNumber,
       date,
     };
     expect(fortunes.map((f) => f.fortune)).toContain(data.fortune);
     expect(commentsMap[data.fortune]).toContain(data.comment);
     expect(luckyColors).toContain(data.lucky_color);
     expect(luckyItems).toContain(data.lucky_item);
+    expect(luckyFoods).toContain(data.lucky_food);
+    expect(luckyNumbers).toContain(data.lucky_number);
     expect(() => OmikujiResponseSchema.parse(data)).not.toThrow();
   });
 });
